b648fa7548df3a21a1617ee2343a50d169f6695e
Andreas Staecker <andreas.staecker@t-online.de>
2018-08-25 11:56:23 +0200

    Iteration 1 abgeschlossen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookStoreService } from './shared/book-store.service';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [BookStoreService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
